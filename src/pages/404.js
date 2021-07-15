1214cbbe87901e918afb019b1c89e6b8a688efd2
Tom Peirs <peirstom@gmail.com>
2021-07-15 21:27:45 +0200

    Adding 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,23 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
+import { StaticImage } from "gatsby-plugin-image"
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <div className="bg-indigo-900 relative overflow-hidden h-screen">
+      <StaticImage className="object-cover object-center rounded-lg " alt="Astronaut lonely on mars" src="../images/tom.jpg"></StaticImage>
+      <div className="inset-0 bg-black opacity-25 absolute">
+      </div>
+      <div className="container mx-auto px-6 md:px-12 relative z-10 flex items-center py-32 xl:py-40">
+        <div className="w-full font-mono flex flex-col items-center relative z-10">
+          <h1 className="font-extrabold text-5xl text-center text-white leading-tight mt-4">
+            You&#x27;re alone here
+          </h1>
+          <p className="font-extrabold text-8xl my-44 text-white animate-bounce">
+            404
+          </p>
+        </div>
+      </div>
+    </div>
+
   )
 }
 
