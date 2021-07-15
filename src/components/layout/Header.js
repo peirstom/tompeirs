3b0d60b8f22f49445c5eaeab9f91326eaf736175
Tom Peirs <peirstom@gmail.com>
2021-07-15 18:41:17 +0200

    Removing the logo Icon

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
-import LogoIcon from '../../svg/LogoIcon';
+// import LogoIcon from '../../svg/LogoIcon';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
-        <div className="w-12 mr-3">
+        {/* <div className="w-12 mr-3">
           <LogoIcon />
-        </div>
-        Lander
+        </div> */}
+        TP
       </div>
       <div className="flex mt-4 sm:mt-0">
         <AnchorLink className="px-4" href="#features">
