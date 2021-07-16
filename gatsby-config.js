930c95ca5dc119470fd1495458ad195528f9744a
Tom Peirs <peirstom@gmail.com>
2021-07-16 16:00:42 +0200

    successfully coupled NetlifyCMS

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,10 +18,10 @@ module.exports = {
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: "images",
-        path: "./src/images/",
+        path: `${__dirname}/src/images`,
+        name: 'images',
       },
       __key: "images",
     },
@@ -29,11 +29,39 @@ module.exports = {
       resolve: "gatsby-source-filesystem",
       options: {
         name: "pages",
-        path: "./src/pages/",
+        path: `${__dirname}/src/pages`,
       },
       __key: "pages",
     },
     `gatsby-plugin-postcss`,
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-relative-images',
+            options: {
+              name: 'uploads',
+            },
+          },
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 2048,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-copy-linked-files',
+            options: {
+              destinationDir: 'static',
+            },
+          },
+        ],
+      },
+    },
     `gatsby-plugin-netlify-cms`,
   ],
 };
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,5 @@
 // Step 1: Import your component
 import * as React from 'react'
-import { Link } from "gatsby"
 import { StaticImage } from 'gatsby-plugin-image'
 
 
